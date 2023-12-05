4f6bd53fb8305752b866a8112e13ddb9d7036b26
zek <105883248+zek-c@users.noreply.github.com>
2023-12-05 13:25:50 -0800

    ctrl z

--- new-tab/fullscreen-controller.js
+++ new-tab/fullscreen-controller.js
@@ -1,32 +1,48 @@
 class FullscreenController {
   constructor(element) {
-    document.addEventListener('keydown', this.onKeyDown.bind(this));
+    element.addEventListener('click', this.onClicked.bind(this));
+
+    this.icons = Array.from(element.children);
+    this.showProperIcon()
   }
 
-  async onKeyDown(event) {
-    if (event.key === 'F3') {
-      const isFullscreen = await this.getFullscreen();
-      this.setFullscreen(!isFullscreen);
+  async onClicked() {
+    if (await this.getFullscreen()) {
+      this.showIcon(0)
+      this.setFullscreen(false);
+    } else {
+      this.showIcon(1)
+      this.setFullscreen(true);
     }
+  }
+
+  async showProperIcon() {
+    let index = await this.getFullscreen() ? 1 : 0
+    this.showIcon(index) // a bit repetitive
+  }
+
+  showIcon(index) {
+    this.icons.forEach(e => e.style.display = "none")
+    this.icons[index].style.display = "block"
   }
 
   setFullscreen(bool) {
     chrome.windows.getLastFocused((window) => {
-      const state = bool
+      let state = bool
         ? chrome.windows.WindowState.MAXIMIZED
-        : chrome.windows.WindowState.NORMAL;
-      chrome.windows.update(window.id, { state });
-    });
+        : chrome.windows.WindowState.NORMAL
+      chrome.windows.update(window.id, { state })
+    })
   }
 
-  async getFullscreen() {
+  getFullscreen() {
     return new Promise((resolve) => {
       chrome.windows.getLastFocused((window) => {
-        const bool = window.state === chrome.windows.WindowState.MAXIMIZED;
-        resolve(bool);
-      });
-    });
+        let bool = window.state === chrome.windows.WindowState.MAXIMIZED
+        resolve(bool)
+      })
+    })
   }
 }
 
-export { FullscreenController };
+export { FullscreenController }
